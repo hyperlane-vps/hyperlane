4ab3a503abaca5603c1777e3677a4f3e095a275c
luxcorvin <luxcorvin@gmail.com>
2026-01-28 09:06:29 -0500

    main.go

--- cmd/hyperlane/main.go
+++ cmd/hyperlane/main.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"time"
+
+	"github.com/<your-username>/hyperlane/internal/controlplane"
+	"github.com/<your-username>/hyperlane/internal/state"
+)
 
 func main() {
-    fmt.Println("Hyperlane control plane starting...")
-    // TODO: load desired/observed state
-    // TODO: reconcile loop
+	store, err := state.NewStateStore("hyperlane.db")
+	if err != nil {
+		log.Fatalf("Failed to open state store: %v", err)
+	}
+	defer store.Close()
+
+	reconciler := controlplane.NewReconciler(store, 5*time.Second)
+	go reconciler.Start()
+
+	// Keep control plane running
+	select {}
 }
